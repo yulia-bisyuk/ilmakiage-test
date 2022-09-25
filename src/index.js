9ebcb6b2d8486b9005af2b365dc2b2443f172b05
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-09-25 20:53:39 +0300

    task finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './sass/main.scss';
+//JavaScript
+const logo = document.getElementById('logo');
+const button = document.getElementById('target');
+
+button.addEventListener('click', changeLogo);
+
+function changeLogo() {
+  logo.src = 'logo-new.95e3e92f.png';
+}
