9bb1a7a383fae735f0c9c0a9dbbc021b1cd86716
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-09-25 21:16:14 +0300

    bug fixes

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const button = document.getElementById('target');
 button.addEventListener('click', changeLogo);
 
 function changeLogo() {
-  logo.src = 'logo-new.95e3e92f.png';
+  logo.setAttribute('src', 'images/logo-new.png');
+  console.log(logo.src);
 }
