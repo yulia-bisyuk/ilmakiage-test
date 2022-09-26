3adf772bad359619fff4d0c2e4a9db69ea101f04
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-09-26 13:01:02 +0300

    bug fixes, logo-toggle added, JQuery added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+//JQuery
+
+const logo = $('#logo');
+
+$('#target').click(function () {
+  logo.attr(
+    'src',
+    logo[0].currentSrc ===
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+      ? 'https://www.ilmakiage.com/skin/frontend/ilmakiage/default/images/logo_new.svg'
+      : 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+  );
+});
+
 //JavaScript
-const logo = document.getElementById('logo');
-const button = document.getElementById('target');
 
-button.addEventListener('click', changeLogo);
+// const logo = document.getElementById('logo');
+// const button = document.getElementById('target');
 
-function changeLogo() {
-  logo.setAttribute('src', 'images/logo-new.png');
-  console.log(logo.src);
-}
+// button.addEventListener('click', changeLogo);
+
+// function changeLogo() {
+//   logo.setAttribute(
+//     'src',
+//     logo.src ===
+//       'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+//       ? 'https://www.ilmakiage.com/skin/frontend/ilmakiage/default/images/logo_new.svg'
+//       : 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+//   );
+// }
